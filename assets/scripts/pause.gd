4e012e8d3173c08f65d28674bfb088c0349a4d4a
lsuto22 <162358308+lsuto22@users.noreply.github.com>
2026-06-24 16:32:32 +0200

    Create pause.gd

---
+++ assets/scripts/pause.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	visible = false
+	get_tree().paused = false
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("pause"):
+		if get_tree().paused:
+			visible = false
+			get_tree().paused = false
+		else:
+			visible = true
+			get_tree().paused = true
+
+func _on_continue_pressed() -> void:
+	get_tree().paused = false
+	hide()
+
+func _on_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
