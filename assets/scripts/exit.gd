7499e65c26cb4dca917da744670408e98905cb7f
lsuto22 <162358308+lsuto22@users.noreply.github.com>
2026-06-28 20:06:19 +0200

    Update exit.gd

--- assets/scripts/exit.gd
+++ assets/scripts/exit.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+@onready var hover: AudioStreamPlayer = $Control/hover
+
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_menu_mouse_entered() -> void:
+	hover.play()
+
+func _on_quit_mouse_entered() -> void:
+	hover.play()
