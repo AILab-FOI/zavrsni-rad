ab2369fc7d4e3e6bc39d5171ea8414325d7542b0
lsuto22 <162358308+lsuto22@users.noreply.github.com>
2026-06-28 20:07:09 +0200

    Update heart_pickup.gd

--- assets/scripts/heart_pickup.gd
+++ assets/scripts/heart_pickup.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@onready var pickup_heart: AudioStreamPlayer = $"../pickup_heart"
+
 func _on_body_entered(body):
 	if body.has_method("heal") and body.health < body.max_health:
 		body.heal(1)
+		pickup_heart.play()
 		queue_free()
