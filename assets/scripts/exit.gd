07ad7cca258b059ff402f0e4889204a78dc2fbdc
lsuto22 <162358308+lsuto22@users.noreply.github.com>
2026-06-29 18:26:07 +0200

    Update exit.gd

--- assets/scripts/exit.gd
+++ assets/scripts/exit.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 @onready var hover: AudioStreamPlayer = $Control/hover
+@onready var time: Label = $Control/Label2
+@onready var best_time: Label = $Control/Label3
+
+func _ready():
+	time.text = "Ukupno vrijeme: " + Gamedata.final_time
+	best_time.text = "Najbolje vrijeme: " + Gamedata.best_time
 
 func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
